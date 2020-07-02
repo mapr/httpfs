59fb2bcb90f71b9b19031ea782d3c779d5324ba2
Xiao Chen <xiao@apache.org>
2020-07-02 16:58:18 +0300

    HDFS-10860. Switch HttpFS from Tomcat to Jetty. Contributed  by John Zhuge.

--- src/main/sbin/httpfs.sh
+++ src/main/sbin/httpfs.sh
@@ -23,75 +23,53 @@ export BASEMAPR=${MAPR_HOME:-/opt/mapr}
 env=${BASEMAPR}/conf/env.sh
 [ -f $env ] && . $env
 
-# resolve links - $0 may be a softlink
-PRG="${0}"
 
-while [ -h "${PRG}" ]; do
-  ls=`ls -ld "${PRG}"`
-  link=`expr "$ls" : '.*-> \(.*\)$'`
-  if expr "$link" : '/.*' > /dev/null; then
-    PRG="$link"
-  else
-    PRG=`dirname "${PRG}"`/"$link"
-  fi
-done
+MYNAME="${0##*/}"
 
-BASEDIR=`dirname ${PRG}`
-BASEDIR=`cd ${BASEDIR}/..;pwd`
+## @description  Print usage
+## @audience     private
+## @stability    stable
+## @replaceable  no
+function print_usage
+{
+  cat <<EOF
+Usage: ${MYNAME} run|start|status|stop
+commands:
+  run     Run HttpFS server, the HDFS HTTP Gateway
+  start   Start HttpFS server as a daemon
+  status  Return the status of the HttpFS server daemon
+  stop    Stop the HttpFS server daemon
+EOF
+}
+echo "WARNING: ${MYNAME} is deprecated," \
+  "please use 'hdfs [--daemon start|status|stop] httpfs'." >&2
 
-source ${HADOOP_LIBEXEC_DIR:-${BASEDIR}/libexec}/httpfs-config.sh
 
-if test -z ${JAVA_HOME}; then
-  JAVA_BIN=java
+if [[ $# = 0 ]]; then
+  print_usage
+  exit
+fi
+
+case $1 in
+  run)
+    args=("httpfs")
+  ;;
+  start|stop|status)
+    args=("--daemon" "$1" "httpfs")
+  ;;
+  *)
+    echo "Unknown sub-command \"$1\"."
+    print_usage
+    exit 1
+  ;;
+esac
+
+# Locate bin
+if [[ -n "${HADOOP_HOME}" ]]; then
+  bin="${HADOOP_HOME}/bin"
 else
-  JAVA_BIN=${JAVA_HOME}/bin/java
-fi
-WEB_APP_HOME=${HTTPFS_CATALINA_HOME}/webapps/webhdfs/WEB-INF
-${JAVA_BIN} -cp ${WEB_APP_HOME}/classes:${WEB_APP_HOME}/lib/*:${MAPR_HOME}/lib/* org.apache.hadoop.lib.util.SSLVariablesUtil updateSslConf ${HTTPFS_CATALINA_HOME}/conf/server.xml 2>>/dev/null
-
-
-# The Java System property 'httpfs.http.port' it is not used by HttpFS,
-# it is used in Tomcat's server.xml configuration file
-#
-export CATALINA_OPTS="${CATALINA_OPTS} -Djava.library.path=/opt/mapr/lib -Dhttpfs.proxyuser.mapred.skip.reduce.max.skip.hosts=0 -Dhttpfs.proxyuser.mapred.skip.reduce.max.skip.groups=0"
-
-catalina_opts="-Dhttpfs.home.dir=${HTTPFS_HOME}";
-catalina_opts="${catalina_opts} -Dhttpfs.config.dir=${HTTPFS_CONFIG}";
-catalina_opts="${catalina_opts} -Dhttpfs.log.dir=${HTTPFS_LOG}";
-catalina_opts="${catalina_opts} -Dhttpfs.temp.dir=${HTTPFS_TEMP}";
-catalina_opts="${catalina_opts} -Dhttpfs.admin.port=${HTTPFS_ADMIN_PORT}";
-catalina_opts="${catalina_opts} -Dhttpfs.http.port=${HTTPFS_HTTP_PORT}";
-catalina_opts="${catalina_opts} -Dhttpfs.http.hostname=${HTTPFS_HTTP_HOSTNAME}";
-catalina_opts="${catalina_opts} -Dhttpfs.sslEnabledProtocols=${HTTPFS_SSL_ENABLED_PROTOCOL}";
-
-if [ -f "${HTTPFS_HOME}"/etc/hadoop/isSecure ] ; then
-  if grep --quiet  secure=true "${HTTPFS_HOME}"/etc/hadoop/isSecure; then
-    catalina_opts="${catalina_opts} -Dhttpfs.hadoop.authentication.type=multiauth";
-    catalina_opts="${catalina_opts} -Dhttpfs.authentication.type=multiauth";
-  fi
-else
-  if grep --quiet  secure=true $MAPR_HOME/conf/mapr-clusters.conf; then
-    catalina_opts="${catalina_opts} -Dhttpfs.hadoop.authentication.type=multiauth";
-    catalina_opts="${catalina_opts} -Dhttpfs.authentication.type=multiauth";
-  fi
+  sbin=$(cd -P -- "$(dirname -- "$0")" >/dev/null && pwd -P)
+  bin=$(cd -P -- "${sbin}/../bin" >/dev/null && pwd -P)
 fi
 
-print "Adding to CATALINA_OPTS:     ${catalina_opts}"
-#DEBUG_OPTS="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=14001,suspend=y"
-
-export CATALINA_OPTS="${CATALINA_OPTS} ${catalina_opts} ${MAPR_ECOSYSTEM_LOGIN_OPTS}"
-print "Using   CATALINA_OPTS:       ${CATALINA_OPTS}"
-
-# A bug in catalina.sh script does not use CATALINA_OPTS for stopping the server
-#
-if [ "${1}" = "stop" ]; then
-  export MAPR_HOME
-  export JAVA_OPTS=${CATALINA_OPTS}
-fi
-
-if [ "${HTTPFS_SILENT}" != "true" ]; then
-  exec ${HTTPFS_CATALINA_HOME}/bin/catalina.sh "$@"
-else
-  exec ${HTTPFS_CATALINA_HOME}/bin/catalina.sh "$@" > /dev/null
-fi
-
+exec "${bin}/hdfs" "${args[@]}"
